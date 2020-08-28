828edfbfa999faf72a96c6baec2998c9763b6a76
Rayjens Code <rayjenscode@gmail.com>
2020-08-28 19:27:23 +0800

    troubleshooting 2nd swiper

--- js/script.js
+++ js/script.js
@@ -1,34 +1,223 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 6.2,
-    spaceBetween: 3,
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2.2,
-            spaceBetween: 3,
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 2.2,
-            spaceBetween: 3,
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 4.2,
-            spaceBetween: 3,
-        },
+// let singleBreakpoints = {
+//     // when window width is >= 320px
+//     120: {
+//         slidesPerView: 1.3,
+//         spaceBetween: 3,
+//     },
+//     // when window width is >= 480px
+//     400: {
+//         slidesPerView: 2.2,
+//         spaceBetween: 3,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//         slidesPerView: 3.1,
+//         spaceBetween: 3,
+//     },
 
-        900: {
-            slidesPerView: 5.2,
+//     900: {
+//         slidesPerView: 4.1,
+//         spaceBetween: 3,
+//     },
+
+//     1090: {
+//         slidesPerView: 5.1,
+//         spaceBetween: 3,
+//     },
+//     1400: {
+//         slidesPerView: 6.1,
+//         spaceBetween: 3,
+//     },
+// };
+
+// let multiRowBreakpoints = {
+//     // when window width is >= 320px
+//     120: {
+//         slidesPerView: 1.3,
+//         spaceBetween: 3,
+//     },
+//     // when window width is >= 480px
+//     400: {
+//         slidesPerView: 1.3,
+//         spaceBetween: 3,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//         slidesPerView: 2.1,
+//         spaceBetween: 3,
+//     },
+
+//     1400: {
+//         slidesPerView: 3,
+//         spaceBetween: 3,
+//     },
+// };
+
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 2.1,
+//     spaceBetween: 3,
+//     breakpoints: {
+//         // when window width is >= 320px
+//         120: {
+//             slidesPerView: 1.3,
+//             spaceBetween: 3,
+//         },
+//         // when window width is >= 480px
+//         400: {
+//             slidesPerView: 2.2,
+//             spaceBetween: 3,
+//         },
+//         // when window width is >= 640px
+//         640: {
+//             slidesPerView: 3.1,
+//             spaceBetween: 3,
+//         },
+
+//         900: {
+//             slidesPerView: 4.1,
+//             spaceBetween: 3,
+//         },
+
+//         1090: {
+//             slidesPerView: 5.1,
+//             spaceBetween: 3,
+//         },
+//         1400: {
+//             slidesPerView: 6.1,
+//             spaceBetween: 3,
+//         },
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+// });
+
+// var swiper2 = new Swiper('.swiper-container-2', {
+//     slidesPerView: 2.1,
+//     spaceBetween: 3,
+//     breakpoints: {
+//         // when window width is >= 320px
+//         120: {
+//             slidesPerView: 1.3,
+//             spaceBetween: 3,
+//         },
+//         // when window width is >= 480px
+//         400: {
+//             slidesPerView: 1.3,
+//             spaceBetween: 3,
+//         },
+//         // when window width is >= 640px
+//         640: {
+//             slidesPerView: 2.1,
+//             spaceBetween: 3,
+//         },
+
+//         1400: {
+//             slidesPerView: 3,
+//             spaceBetween: 3,
+//         },
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+// });
+
+const slides = document.querySelectorAll('.swiper-container');
+
+slides.forEach((x) => {
+    let dataNum = x.getAttribute('data-num');
+
+    if (dataNum === 'A') {
+        console.log(dataNum);
+        x = new Swiper('.swiper-container', {
+            slidesPerView: 2.1,
             spaceBetween: 3,
-        },
-        1400: {
-            slidesPerView: 6.2,
+            breakpoints: {
+                120: {
+                    slidesPerView: 1.3,
+                    spaceBetween: 3,
+                },
+                // when window width is >= 480px
+                400: {
+                    slidesPerView: 2.2,
+                    spaceBetween: 3,
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 3.1,
+                    spaceBetween: 3,
+                },
+
+                900: {
+                    slidesPerView: 4.1,
+                    spaceBetween: 3,
+                },
+
+                1090: {
+                    slidesPerView: 5.1,
+                    spaceBetween: 3,
+                },
+                1400: {
+                    slidesPerView: 6.1,
+                    spaceBetween: 3,
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    }
+
+    if (dataNum === 'B') {
+        console.log(dataNum);
+        x = new Swiper('.swiper-container', {
+            slidesPerView: 2.1,
             spaceBetween: 3,
-        },
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+            breakpoints: {
+                // when window width is >= 320px
+                120: {
+                    slidesPerView: 1.3,
+                    spaceBetween: 3,
+                },
+                // when window width is >= 480px
+                400: {
+                    slidesPerView: 1.3,
+                    spaceBetween: 3,
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 2.1,
+                    spaceBetween: 3,
+                },
+
+                1400: {
+                    slidesPerView: 3,
+                    spaceBetween: 3,
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    }
 });
